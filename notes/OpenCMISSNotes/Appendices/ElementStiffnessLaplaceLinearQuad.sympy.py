16af0ecf4f5bf3e8288fb7bfc295ccfefc3cfa76
Chris Bradley <c.bradley@auckland.ac.nz>
2026-03-16 19:15:32 +1300

    Update

--- notes/OpenCMISSNotes/Appendices/ElementStiffnessLaplaceLinearQuad.sympy.py
+++ notes/OpenCMISSNotes/Appendices/ElementStiffnessLaplaceLinearQuad.sympy.py
@@ -6,7 +6,20 @@ numberOfNodes = 4
 
 dx, dy = symbols('dx,dy')
 xi1, xi2  = symbols('xi1,xi2')
+k = symbols('k')
 
+K = zeros(numberOfNodes,numberOfNodes)
+KE = zeros(numberOfNodes,numberOfNodes)
+
+def Evaluate(Object):
+    return Object.evalf(subs={dx:3/4,dy:1/4,k:-0.01})
+
+def PrintMatrix(Mat,Name):
+    print("")
+    for iIdx in range(0,numberOfNodes):
+        for jIdx in range(0,numberOfNodes):
+            print(" %s[%2d,%2d] = %s" % (Name,iIdx+1,jIdx+1,simplify(Mat[iIdx,jIdx])))
+            
 dPhidxi = Matrix([[ -(1-xi2), -(1-xi1) ],
                   [   1-xi2 ,    -xi1  ],
                   [    -xi2 ,   1-xi1  ], 
@@ -31,9 +44,15 @@ for mIdx in range(0,numberOfNodes):
          for xiIdx1 in range(0,numberOfXi):
              for xiIdx2 in range(0,numberOfXi):
                  integrand = integrand + dPhidxi[mIdx,xiIdx1]*dPhidxi[nIdx,xiIdx2]*gu[xiIdx1,xiIdx2]
-         integrand = integrand*J
+         integrand = k*integrand*J
          integral1 = integrate(integrand,(xi2,0,1))
          integral2 = simplify(integrate(integral1,(xi1,0,1)))
-         print("m=%2d, n=%2d, K = %s" % (mIdx+1,nIdx+1,integral2))
+         K[mIdx,nIdx] = integral2
+         
+                 
+KE = Evaluate(K)
+
+PrintMatrix(K,'K')
+PrintMatrix(KE,'KE')
                  
 
--- notes/OpenCMISSNotes/Appendices/MassStiffnessLinearQuad.sympy.py
+++ notes/OpenCMISSNotes/Appendices/MassStiffnessLinearQuad.sympy.py
@@ -6,18 +6,34 @@ numberOfNodes = 4
 
 dx, dy = symbols('dx,dy')
 xi1, xi2  = symbols('xi1,xi2')
+k = symbols('k')
 
+C = zeros(numberOfNodes,numberOfNodes)
+CE = zeros(numberOfNodes,numberOfNodes)
+
+def Evaluate(Object):
+    return Object.evalf(subs={dx:3/4,dy:1/4,k:1})
+
+def PrintMatrix(Mat,Name):
+    print("")
+    for iIdx in range(0,numberOfNodes):
+        for jIdx in range(0,numberOfNodes):
+            print(" %s[%2d,%2d] = %s" % (Name,iIdx+1,jIdx+1,simplify(Mat[iIdx,jIdx])))
+            
 Phi = Matrix([[ (1-xi1)*(1-xi2) ],
                   [ xi1*(1-xi2) ],
                   [ (1-xi1)*xi2 ], 
-                  [ xi1*xi1 ]])
+                  [ xi1*xi2 ]])
 J = dx*dy
 
 for mIdx in range(0,numberOfNodes):
      for nIdx in range(0,numberOfNodes):
-         integrand = Phi[mIdx]*Phi[nIdx]*J
+         integrand = k*Phi[mIdx]*Phi[nIdx]*J
          integral1 = integrate(integrand,(xi2,0,1))
          integral2 = simplify(integrate(integral1,(xi1,0,1)))
-         print("m=%2d, n=%2d, K = %s" % (mIdx+1,nIdx+1,integral2))
+         C[mIdx,nIdx] = integral2
                  
+CE = Evaluate(C)
 
+PrintMatrix(C,'C')
+PrintMatrix(CE,'CE')
